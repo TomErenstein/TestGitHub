9d61270942ac9d082f9944d22b5baaccaa80ac01
Tom Erenstein <tom.eerenstein@gmail.com>
2018-11-19 18:02:26 +0100

    Button Script

---
+++ Assets/Scripts/navigateToScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigateToScene : MonoBehaviour {
+
+	[SerializeField]
+
+	string SceneToLoad;
+
+	public void LoadNextScene() {
+		SceneManager.LoadScene(SceneToLoad);
+	}
+}
